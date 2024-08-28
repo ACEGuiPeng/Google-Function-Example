b257cd9cb8ead2afbb1f750c554a35e2f1d4f856
guipeng <aceguipeng@gmail.com>
2024-08-29 00:41:21 +0800

    complete api proxy demo

--- src/main.py
+++ src/main.py
@@ -6,29 +6,42 @@
 @Date  : 2024/8/28
 @Desc :
 """
-import functions_framework
 
-from markupsafe import escape
+import functions_framework
+import requests
+from flask import Request, make_response
 
 
 @functions_framework.http
-def api_proxy(request):
-    """HTTP Cloud Function.
-    Args:
-        request (flask.Request): The request object.
-        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-    Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+def api_proxy(request: Request):
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
+        1 Get target url
+        2 Get proxy method
+        3 get data body
+        4 proxy to target url with params
+    :param request:
+    :return:
+    """
+    target_url = request.path.replace("/", "", 1)
+    if not target_url.startswith("http"):
+        return resp_failure("Target url must start with http")
 
-    if request_json and "name" in request_json:
-        name = request_json["name"]
-    elif request_args and "name" in request_args:
-        name = request_args["name"]
-    else:
-        name = "World"
-    return f"Hello {escape(name)}!"
+    method = request.method
+    params = request.args.to_dict()
+    data = request.data
+    headers = dict(request.headers)
+    # remove host to avoid 403
+    headers.pop("Host", None)
+
+    cookies = request.cookies
+    print(
+        f"Try to proxy request: {target_url}, method: {method}, params: {params}, data: {data}, headers: {headers},cookies: {cookies}")
+    resp = requests.request(method=method, url=target_url, data=data, params=params,
+                            headers=headers, cookies=cookies)
+    flask_response = make_response(resp.content, resp.status_code)
+    print(f"SUCCESS to get proxy request result from server: {target_url},status: {resp.status_code}")
+    return flask_response
+
+
+def resp_failure(reason: str):
+    return {"result": "failure", "reason": reason}
